6270713a3ad1dd0bcc56f62daca6817ef38fa906
soohy <soohy@soohyun>
2025-11-24 23:06:27 +0900

    test

--- movie-site/src/main/java/com/koreait/moviesite/MovieSiteApplication.java
+++ movie-site/src/main/java/com/koreait/moviesite/MovieSiteApplication.java
@@ -7,5 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MovieSiteApplication {
     public static void main(String[] args) {
         SpringApplication.run(MovieSiteApplication.class, args);
-    }
+    }ss
 }

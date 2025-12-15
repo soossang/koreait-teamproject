9c0dab0c943946648bc36cf7b49cc2df721a189e
YONGHEEIM <qldtks777@gmail.com>
2025-12-15 17:49:00 +0900

    12/15 임용희(학원, 오후 5시 48분)

--- movie-site/src/main/java/com/koreait/moviesite/RankingGenreBoard/controller/AlltimeController.java
+++ movie-site/src/main/java/com/koreait/moviesite/RankingGenreBoard/controller/AlltimeController.java
@@ -14,17 +14,16 @@ public class AlltimeController {
 
     private final AlltimeService service;
 
-    // 기존: /api/boxoffice/alltime?limit=...&sortBy=...&dir=...
-    // public ResponseEntity<List<BoxOfficeSummary>> alltime(...)
+    public AlltimeController(AlltimeService service) {
+        this.service = service;
+    }
 
-    // ★ 새로 추가: 페이지네이션 버전
-    // 예) /api/boxoffice/alltime-page?page=1&size=30&sortBy=audi&dir=desc
     @GetMapping("/alltime-page")
     public ResponseEntity<Page<BoxOfficeSummary>> alltimePage(
-            @RequestParam(name="page",  defaultValue="1")  int page,
-            @RequestParam(name="size",  defaultValue="30") int size,
-            @RequestParam(name="sortBy",defaultValue="audi") String sortBy,
-            @RequestParam(name="dir",   defaultValue="desc") String dir) {
+            @RequestParam(value="page", defaultValue="1") int page,
+            @RequestParam(value="size", defaultValue="30") int size,
+            @RequestParam(value="sortBy", defaultValue="audi") String sortBy,
+            @RequestParam(value="dir", defaultValue="desc") String dir) {
 
         return ResponseEntity.ok(service.page(page, size, sortBy, dir));
     }
--- movie-site/src/main/java/com/koreait/moviesite/RankingGenreBoard/controller/HomeController.java
+++ movie-site/src/main/java/com/koreait/moviesite/RankingGenreBoard/controller/HomeController.java
@@ -8,7 +8,6 @@ public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        // templates/RankingGenreboard/ranking.html
-        return "RankingGenreboard/ranking";
+        return "redirect:/movies";
     }
 }
